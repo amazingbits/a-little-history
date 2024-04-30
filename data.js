5e6f937770d1a222924ecec29ef2948ebfede8dc
Guilherme Andrade <gui-keanne@hotmail.com>
2024-04-29 21:09:17 -0300

    ajustando os dados

--- data.js
+++ data.js
@@ -1,192 +1,234 @@
 const data = [
-    {
-        image: "/images/boy.png",
-        animated: true,
-        bg: "city-rain-1.jpg",
-        text: "O ano era 2022, acho que Agosto..."
-    },
-    {
-        image: "/images/boy.png",
-        animated: false,
-        bg: "city-rain-1.jpg",
-        text: "Lembro do frio, da chuva e de todos os motivos do mundo pra ficar em casa..."
-    },
-    {
-        image: "/images/umbrella-girl.png",
-        animated: true,
-        bg: "city-rain.jpg",
-        text: "Após quinze minutos de tensão e espera, te avisto pela primeira vez..."
-    },
-    {
-        image: "/images/umbrella-girl.png",
-        animated: false,
-        bg: "city-rain.jpg",
-        text: "Um metro e meio de graça e ternura (que mais tarde se mostrou ser apenas a primeira impressão)..."
-    },
-    {
-        image: "/images/couple.png",
-        animated: true,
-        bg: "bar.jpg",
-        text: "Por fora mostrávamos descontração..."
-    },
-    {
-        image: "/images/couple.png",
-        animated: false,
-        bg: "bar.jpg",
-        text: "O raciocínio rápido para piadas infames era mútuo..."
-    },
-    {
-        image: "/images/black-three.gif",
-        animated: true,
-        bg: "dark-forest.jpg",
-        text: "Mas o que nos uniu foi o que tínhamos por dentro..."
-    },
-    {
-        image: "/images/black-three.gif",
-        animated: false,
-        bg: "dark-forest.jpg",
-        text: "Muito diferentes, mas iguais nas coisas certas..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: true,
-        bg: "sea.jpg",
-        text: "A vida ao seu lado trouxe uma série de emoções..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: false,
-        bg: "sea.jpg",
-        text: "Discutimos..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: false,
-        bg: "sea.jpg",
-        text: "Refletimos..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: false,
-        bg: "sea.jpg",
-        text: "Aprendemos..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: false,
-        bg: "sea.jpg",
-        text: "E continuamos aprendendo..."
-    },
-    {
-        image: "/images/couple-1.png",
-        animated: true,
-        bg: "farm.jpg",
-        text: "Tivemos dias incríveis..."
-    },
-    {
-        image: "/images/certification.png",
-        animated: true,
-        bg: "college.jpg",
-        text: "Conquistas..."
-    },
-    {
-        image: "/images/winged-dog.png",
-        animated: true,
-        bg: "heaven.jpg",
-        text: "Perdas..."
-    },
-    {
-        image: "/images/matilda.png",
-        animated: true,
-        bg: "grass.jpg",
-        text: "Recomeços..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: true,
-        bg: "sea.jpg",
-        text: "Tudo é incerto com a gente..."
-    },
-    {
-        image: "/images/windrose.gif",
-        animated: false,
-        bg: "sea.jpg",
-        text: "Mas a única certeza que eu tenho..."
-    },
-    {
-        image: "/images/ifelse.png",
-        animated: true,
-        bg: "tech.jpg",
-        text: "É que, por mais que os nossos dias sejam como um caldeirão repleto de variáveis..."
-    },
-    {
-        image: "/images/ifelse.png",
-        animated: false,
-        bg: "tech.jpg",
-        text: "A constante é que no fim do dia, eu sempre te amo..."
-    },
-    {
-        image: "/images/cvv.png",
-        animated: true,
-        bg: "blue.jpg",
-        text: "Essa é a mensagem que nós, da CVV, desejamos a você..."
-    },
-    {
-        image: "/images/cvv.png",
-        animated: false,
-        bg: "blue.jpg",
-        text: "Brinks..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: true,
-        bg: "love.jpg",
-        text: "Eu te amo..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: false,
-        bg: "love.jpg",
-        text: "É sério..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: false,
-        bg: "love.jpg",
-        text: "A CVV também te ama, a propósito..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: false,
-        bg: "love.jpg",
-        text: "Mas eu amo mais..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: false,
-        bg: "love.jpg",
-        text: "Olha que coração bonitinho..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: false,
-        bg: "love.jpg",
-        text: "É isso... Bom trabalho e um excelente dia..."
-    },
-    {
-        image: "/images/beating-heart.gif",
-        animated: false,
-        bg: "love.jpg",
-        text: "PS: esqueci de lavar o fogão...",
-        finish: true,
-    },
-    {
-        image: "/images/dove.png",
-        animated: true,
-        bg: "soccer.jpg",
-        text: "#paznosestadios",
-        finish: true,
-    },
+	{
+		image: '/images/boy.png',
+		animated: true,
+		bg: 'city-rain-1.jpg',
+		text: 'O ano era 2022, acho que Agosto...',
+	},
+	{
+		image: '/images/boy.png',
+		animated: false,
+		bg: 'city-rain-1.jpg',
+		text: 'Lembro do frio, da chuva...',
+	},
+	{
+		image: '/images/boy.png',
+		animated: false,
+		bg: 'city-rain-1.jpg',
+		text: 'Tínhamos tudo pra ficar em casa...',
+	},
+	{
+		image: '/images/umbrella-girl.png',
+		animated: true,
+		bg: 'city-rain.jpg',
+		text: 'E você aparece...',
+	},
+	{
+		image: '/images/umbrella-girl.png',
+		animated: false,
+		bg: 'city-rain.jpg',
+		text: 'Um metro e meio de graça e ternura...',
+	},
+	{
+		image: '/images/umbrella-girl.png',
+		animated: false,
+		bg: 'city-rain.jpg',
+		text: 'Que foram embora logo no início...',
+	},
+	{
+		image: '/images/umbrella-girl.png',
+		animated: false,
+		bg: 'city-rain.jpg',
+		text: 'Dando lugar a uma pedreirinha em potencial...',
+	},
+	{
+		image: '/images/couple.png',
+		animated: true,
+		bg: 'bar.jpg',
+		text: 'Por fora mostrávamos descontração...',
+	},
+	{
+		image: '/images/couple.png',
+		animated: false,
+		bg: 'bar.jpg',
+		text: 'A habilidade mútua para piadas infames...',
+	},
+	{
+		image: '/images/black-three.gif',
+		animated: true,
+		bg: 'dark-forest.jpg',
+		text: 'Mas o que nos uniu foi o que tínhamos por dentro...',
+	},
+	{
+		image: '/images/black-three.gif',
+		animated: false,
+		bg: 'dark-forest.jpg',
+		text: 'Muito diferentes, mas iguais nas coisas certas...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: true,
+		bg: 'sea.jpg',
+		text: 'A vida ao seu lado trouxe uma série de emoções...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: false,
+		bg: 'sea.jpg',
+		text: 'Discutimos...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: false,
+		bg: 'sea.jpg',
+		text: 'Refletimos...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: false,
+		bg: 'sea.jpg',
+		text: 'Aprendemos...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: false,
+		bg: 'sea.jpg',
+		text: 'E continuamos aprendendo...',
+	},
+	{
+		image: '/images/couple-1.png',
+		animated: true,
+		bg: 'farm.jpg',
+		text: 'Tivemos dias incríveis...',
+	},
+	{
+		image: '/images/couple-1.png',
+		animated: false,
+		bg: 'farm.jpg',
+		text: 'É sério...',
+	},
+	{
+		image: '/images/certification.png',
+		animated: true,
+		bg: 'college.jpg',
+		text: 'Conquistas...',
+	},
+	{
+		image: '/images/certification.png',
+		animated: false,
+		bg: 'college.jpg',
+		text: 'Juntos...',
+	},
+	{
+		image: '/images/winged-dog.png',
+		animated: true,
+		bg: 'heaven.jpg',
+		text: 'Perdas...',
+	},
+	{
+		image: '/images/winged-dog.png',
+		animated: false,
+		bg: 'heaven.jpg',
+		text: 'Difíceis...',
+	},
+	{
+		image: '/images/matilda.png',
+		animated: true,
+		bg: 'grass.jpg',
+		text: 'Recomeços...',
+	},
+	{
+		image: '/images/matilda.png',
+		animated: true,
+		bg: 'grass.jpg',
+		text: 'Um tanto atropelados...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: true,
+		bg: 'sea.jpg',
+		text: 'Quase tudo é incerto com a gente...',
+	},
+	{
+		image: '/images/windrose.gif',
+		animated: false,
+		bg: 'sea.jpg',
+		text: 'Mas a única certeza que eu tenho...',
+	},
+	{
+		image: '/images/ifelse.png',
+		animated: true,
+		bg: 'tech.jpg',
+		text: 'É que, mesmo que hajam muitas variáveis...',
+	},
+	{
+		image: '/images/ifelse.png',
+		animated: false,
+		bg: 'tech.jpg',
+		text: 'A constante é que no fim do dia, eu sempre te amo...',
+	},
+	{
+		image: '/images/cvv.png',
+		animated: true,
+		bg: 'blue.jpg',
+		text: 'Essa é a mensagem que nós, da CVV, desejamos a você...',
+	},
+	{
+		image: '/images/cvv.png',
+		animated: false,
+		bg: 'blue.jpg',
+		text: 'Brinks...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: true,
+		bg: 'love.jpg',
+		text: 'Eu te amo...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: false,
+		bg: 'love.jpg',
+		text: 'É sério...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: false,
+		bg: 'love.jpg',
+		text: 'A CVV também te ama, a propósito...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: false,
+		bg: 'love.jpg',
+		text: 'Mas eu amo mais...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: false,
+		bg: 'love.jpg',
+		text: 'Olha que coração bonitinho...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: false,
+		bg: 'love.jpg',
+		text: 'É isso... Bom trabalho e um excelente dia...',
+	},
+	{
+		image: '/images/beating-heart.gif',
+		animated: false,
+		bg: 'love.jpg',
+		text: 'PS: esqueci de lavar o fogão...',
+		finish: true,
+	},
+	{
+		image: '/images/dove.png',
+		animated: true,
+		bg: 'soccer.jpg',
+		text: '#paznosestadios',
+		finish: true,
+	},
 ];
 
 export default data;
--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,83 @@
 import React from 'react';
-import data from "../data";
-import Button from "../components/Button";
+import data from '../data';
+import Button from '../components/Button';
 import Text from '../components/Text';
 import Image from '../components/Image';
 import ImageWrapper from '../components/ImageWrapper';
 
 function Home() {
-    const [screenText, setScreenText] = React.useState("");
-    const [screenImage, setScreenImage] = React.useState("/images/boy.png");
-    const [screenBg, setScreenBg] = React.useState("city-rain-1.jpg");
-    const [currentTextIndex, setCurrentTextIndex] = React.useState(0);
-    const [isAnimatedImage, setIsAnimatedImage] = React.useState(false);
-    const [dataIndex, setDataIndex] = React.useState(0);
-    const buttonRef = React.useRef(null);
+	const [screenText, setScreenText] = React.useState('');
+	const [screenImage, setScreenImage] = React.useState('/images/boy.png');
+	const [screenBg, setScreenBg] = React.useState('city-rain-1.jpg');
+	const [currentTextIndex, setCurrentTextIndex] = React.useState(0);
+	const [isAnimatedImage, setIsAnimatedImage] = React.useState(false);
+	const [dataIndex, setDataIndex] = React.useState(0);
+	const buttonRef = React.useRef(null);
 
-    const textSpeed = 100;
+	const textSpeed = 100;
 
-    function handleButtonClick() {
-        if(data[dataIndex].finish) {
-            buttonRef.current.remove();
-        }
-        setScreenText("");
-        if(dataIndex < data.length - 1) {
-            setDataIndex(dataIndex + 1);   
-        }
-        changeImage();
-        typeTextEffect(screenText);
-    }
+	function handleButtonClick() {
+		if (data[dataIndex].finish) {
+			buttonRef.current.remove();
+		}
+		setScreenText('');
+		if (dataIndex < data.length - 1) {
+			setDataIndex(dataIndex + 1);
+		}
+		changeImage();
+		typeTextEffect(screenText);
+	}
 
-    function changeImage() {
-        setScreenImage(data[dataIndex].image);
-        setScreenBg(data[dataIndex].bg);
-        setIsAnimatedImage(data[dataIndex].animated);
-    }
-    
-    function changeButtonStatus(ref, isEnabled) {
-        ref.current.disabled = !isEnabled;
-    }
+	function changeImage() {
+		setScreenImage(data[dataIndex].image);
+		setScreenBg(data[dataIndex].bg);
+		setIsAnimatedImage(data[dataIndex].animated);
+	}
 
-    function activateButtonAfterAnimation() {
-        changeButtonStatus(buttonRef, false);
-        const timeout = setTimeout(() => {
-            changeButtonStatus(buttonRef, true);
-            clearTimeout(timeout);
-        }, ((textSpeed * data[dataIndex].text.length)) * 0.3);
-    }
+	function changeButtonStatus(ref, isEnabled) {
+		ref.current.disabled = !isEnabled;
+	}
 
-    function typeTextEffect(text) {
-        let timeout;
-        setCurrentTextIndex(0);
-        if(currentTextIndex < text.length) {
-            timeout = setTimeout(() => {
-                setScreenText(screenText + text[currentTextIndex]);
-                setCurrentTextIndex(currentTextIndex + 1);
-            }, textSpeed);
-        }
+	function activateButtonAfterAnimation() {
+		changeButtonStatus(buttonRef, false);
+		const timeout = setTimeout(() => {
+			changeButtonStatus(buttonRef, true);
+			clearTimeout(timeout);
+		}, textSpeed * data[dataIndex].text.length * 0.3);
+	}
 
-        return () => clearTimeout(timeout);
-    }
+	function typeTextEffect(text) {
+		let timeout;
+		setCurrentTextIndex(0);
+		if (currentTextIndex < text.length) {
+			timeout = setTimeout(() => {
+				setScreenText(screenText + text[currentTextIndex]);
+				setCurrentTextIndex(currentTextIndex + 1);
+			}, textSpeed);
+		}
 
-    React.useEffect(() => {
-        changeImage();
-        typeTextEffect(data[dataIndex].text);
-        if(screenText.length >= data[dataIndex].text.length) {
-            activateButtonAfterAnimation();
-        } else {
-            changeButtonStatus(buttonRef, false);
-        }
-    }, [screenText]);
+		return () => clearTimeout(timeout);
+	}
 
-    return ( 
-    <div className="container">
-        <ImageWrapper backgroundImage={screenBg}>
-            <Image src={screenImage} alt="Matilda" isAnimated={isAnimatedImage} />
-        </ImageWrapper>
-        <Text>{screenText}</Text>
-        <Button onClick={handleButtonClick} title="Avançar" id="btn" ref={buttonRef} />
-    </div>
-    )
+	React.useEffect(() => {
+		changeImage();
+		typeTextEffect(data[dataIndex].text);
+		if (screenText.length >= data[dataIndex].text.length) {
+			activateButtonAfterAnimation();
+		} else {
+			changeButtonStatus(buttonRef, false);
+		}
+	}, [screenText]);
+
+	return (
+		<div className='container'>
+			<ImageWrapper backgroundImage={screenBg}>
+				<Image src={screenImage} alt='Matilda' isAnimated={isAnimatedImage} />
+			</ImageWrapper>
+			<Text>{screenText}</Text>
+			<Button onClick={handleButtonClick} title='Avançar' id='btn' ref={buttonRef} />
+		</div>
+	);
 }
 
 export default Home;
